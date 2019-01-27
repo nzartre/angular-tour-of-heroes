1b445fb447245e8c14649b190642a93d0d344c53
Nathan Yiangsupapaanonntr <zartre.y@gmail.com>
2019-01-27 11:58:09 +0700

    Add click event to hero item

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
